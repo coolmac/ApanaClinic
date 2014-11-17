bef9d4b38fdb1010969f22dd8604e456ea5729f1
Amol Deshmukh <amol_asd@yahoo.com>
2014-11-17 18:42:22 +0530

    home search page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,15 @@
 class Patient < ActiveRecord::Base
+#validates :name, :age, :dob, :mobile, :gender, :address, :confirm_call, presence: true
+#validates :language, :call_time, :call_preference, :ralative_name, :relation_with_relative, :relative_mobile, :disease, :health_status, presence: true
+
+
+#validates_numericality_of :age, :only_integer =>true, 
+                        #  :greater_than_or_equal_to =>0, 
+                        #  :message => "invalid age, it must be positive"
+validates_inclusion_of :health_status, :in => %w(Good Normal Bad Urgent ),
+                        :message => " only allow Good or Bad or Normal"
+
+validates_numericality_of :mobile, :only_integer =>true 
+
+
 end
